a322ce8b58de4bf0dc80c6c4a1d8d17dfcc8cbd2
Tikokito <30938070+tikokito123@users.noreply.github.com>
2021-05-29 03:41:47 +0300

    create a user module

--- server/Models/userModel.js
+++ server/Models/userModel.js
@@ -1,2 +1,34 @@
 const Joi = require('joi');
 const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    admin: {
+        type: Boolean,
+        default: false
+    }
+})
+
+const User = mongoose.model('users', userSchema);
+
+function validateUser(user){
+    const schema = Joi.object({
+        username: Joi.string().required(),
+        email: Joi.string().email().required(),
+        password: Joi.string().required()
+    })
+    return schema.validate(user);
+}
+exports.validateUser = validateUser;
+exports.User = User;
